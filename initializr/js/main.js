fc2f0d256d7f60c4ee19673d8cf7f6d6e6e54ffd
av4sports <AV4@wireless-10-145-109-25.public.utexas.edu>
2016-11-15 19:18:40 -0600

    commit to showing and hiding items

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -2,3 +2,13 @@ function highlight(){
   $('h1').text('Hello');
   $('.jumbotron').addClass('highlight');
 }
+function unhiglight(){
+  $('h1').text('Hello, world');
+  $('.jumbotron').removeClass('highlight');
+}
+function hide(){
+  $('.jumbotron').fadeOut()
+}
+function show(){
+  $('.jumbotron').slideDown()
+}
